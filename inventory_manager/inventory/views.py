6d5e5a4f70f57217dc0b5a2af0a33d410e90dd33
Pedro Thomas <124922008+Prthomas808@users.noreply.github.com>
2026-01-19 10:39:02 -0500

    views and templates added

--- inventory_manager/inventory/views.py
+++ inventory_manager/inventory/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Inventory
+from .forms import InventoryForm
 
 # Create your views here.
+
+# all inventory view
+def all_inventory(request):
+    items = Inventory.objects.all()
+    return render(request, 'inventory_home.html', {'items' : items})
+
+# add item to inventory view
+def add_to_inventory(request):
+    if request.method == "POST":
+        form = InventoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('all_inventory')
+    else:
+        form = InventoryForm()
+
+    return render(request, 'add_to_inventory.html', {'form' : form})
