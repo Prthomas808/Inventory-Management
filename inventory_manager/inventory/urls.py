7e46f062c53ab84bbae7a2652e0b3d28c14980c8
Pedro Thomas <124922008+Prthomas808@users.noreply.github.com>
2026-01-19 13:15:57 -0500

    project reset

--- inventory_manager/inventory/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.all_inventory, name='inventory-home'),
-    path('add/', views.add_to_inventory, name='add_to_inventory')
-]
