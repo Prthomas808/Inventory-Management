00e2c30c14431a119451447d35325390f2cc0b7d
Pedro Thomas <124922008+Prthomas808@users.noreply.github.com>
2026-01-19 13:30:08 -0500

    models and migrations created

--- inventoryApp/models.py
+++ inventoryApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=65)
+    product_sku = models.CharField(max_length=65, unique=True)
+    product_price = models.FloatField()
+    product_quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
